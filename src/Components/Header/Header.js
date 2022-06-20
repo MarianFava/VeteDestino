6fedcb874139261d113c7e29936ffcd2e600e7f4
MarianFava <marianaspano@gmail.com>
2022-06-20 20:09:58 -0300

    Desafio 2 Menu Ecommerce

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import logo from "../../assets/logo2.png"
+import carrito from "../../assets/carrito-de-compras.png"
+import "./Header.css"
+const Header = () =>{
+    return (
+        <header>
+        <img src={logo} className="imgLogo" alt="" />
+        <h1>Vete Destino Indumentaria</h1>
+        <nav>
+            <a href="#">Winter 22</a>
+            <a href="#">Sale</a>
+            <a href="#">Contact Us </a>
+
+        </nav>
+        <img src={carrito} className="imgCarrito" alt="" />
+        </header>
+    )
+}
+
+export default Header
